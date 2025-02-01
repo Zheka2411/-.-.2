e6b7ea43506d72b829063593c53c933c729704cb
Zheka2411 <131461545+Zheka2411@users.noreply.github.com>
2025-02-01 13:49:18 +0000

    Update Student.cpp

--- Конструктори. Деструктор.2/Конструктори. Деструктор.2/Student.cpp
+++ Конструктори. Деструктор.2/Конструктори. Деструктор.2/Student.cpp
@@ -1,12 +1,20 @@
 #include "Student.h"
 
 Student::Student(const char* fn, const char* bd, Contact c, College co) : contact(c), college(co) {
-    int i = 0;
-    while (fn[i] != '\0' && i < 99) { fullName[i] = fn[i]; i++; }
-    fullName[i] = '\0';
-    i = 0;
-    while (bd[i] != '\0' && i < 19) { birthDate[i] = bd[i]; i++; }
-    birthDate[i] = '\0';
+    int len_fn = 0, len_bd = 0;
+    while (fn[len_fn] != '\0') len_fn++;
+    while (bd[len_bd] != '\0') len_bd++;
+
+    fullName = new char[len_fn + 1];
+    birthDate = new char[len_bd + 1];
+
+    for (int i = 0; i <= len_fn; i++) fullName[i] = fn[i];
+    for (int i = 0; i <= len_bd; i++) birthDate[i] = bd[i];
+}
+
+Student::~Student() {
+    delete[] fullName;
+    delete[] birthDate;
 }
 
 void Student::Show() const {
